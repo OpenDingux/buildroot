4aa93653a6816347a55241670e89950e35577ad6
Christophe Branchereau <cbranchereau@gmail.com>
2020-11-08 11:17:02 +0100

    S90volume.sh : also store headphones volume
    A solution for the minimum volume being too loud if only PCM is saved
    
    Signed-off-by: Christophe Branchereau <cbranchereau@gmail.com>

--- board/opendingux/gcw0/overlay/etc/init.d/S90volume.sh
+++ board/opendingux/gcw0/overlay/etc/init.d/S90volume.sh
@@ -4,18 +4,22 @@
 #
 
 VOLUME_STATEFILE=/usr/local/etc/volume.state
-CONTROL=PCM
+PCM=PCM
+HP=Headphones
 
 case "$1" in
 	start)
 		echo "Loading sound volume..."
 		if [ -f $VOLUME_STATEFILE ]; then
-			/usr/bin/amixer set $CONTROL `cat $VOLUME_STATEFILE`
+			/usr/bin/amixer set $PCM `sed -n 's/PCM://p' $VOLUME_STATEFILE`
+			/usr/bin/amixer set $HP `sed -n 's/HP://p' $VOLUME_STATEFILE`
 		fi
 		;;
 	stop)
 		echo "Storing sound volume..."
-		amixer get $CONTROL | sed -n 's/.*Front .*: Playback \([0-9]*\).*$/\1/p' | paste -d "," - - > $VOLUME_STATEFILE
+		PCM_VOL=`amixer get $PCM | sed -n 's/.*Front .*: Playback \([0-9]*\).*$/\1/p' | paste -d "," - -`
+		HP_VOL=`amixer get $HP | sed -n 's/.*Front .*: Playback \([0-9]*\).*$/\1/p' | paste -d "," - -`
+		printf "PCM:$PCM_VOL\nHP:$HP_VOL\n" > $VOLUME_STATEFILE
 		;;
 	*)
 		echo "Usage: $0 {start|stop}"
