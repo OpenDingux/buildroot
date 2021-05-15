a70feede4359ecfb94ecd37784f220df41c8cc69
Sergiy Kukunin <sergey.kukunin@gmail.com>
2021-05-15 17:30:25 +0300

    od: od-update: Give ability for users to select device manually
    Signed-off-by: Sergiy Kukunin <sergey.kukunin@gmail.com>

--- board/opendingux/opk_update_script.sh
+++ board/opendingux/opk_update_script.sh
@@ -4,6 +4,85 @@ cd `dirname $0`
 
 DATE_FILE=./date.txt
 
+export DIALOGOPTS="--colors --no-shadow"
+if [ -f "$DATE_FILE" ] ; then
+	DATE="`cat $DATE_FILE`"
+	export DIALOGOPTS="$DIALOGOPTS --backtitle \"OpenDingux update $DATE\""
+fi
+
+echo "screen_color = (RED,RED,ON)" > /tmp/dialog_err.rc
+
+# ----------
+# 1. Select and confirm device
+# ----------
+THISDEVICE=./thisdevice
+DEVICE=$($THISDEVICE --detect)
+while true; do
+	if [ -n "$DEVICE" ]; then
+		eval $($THISDEVICE --device $DEVICE | grep DEVICE_NAME)
+		if [ -z "$DEVICE_NAME" ]; then
+			DEVICE=
+			continue
+		fi
+		DEVICE_LINE="
+\Zb\Z3$DEVICE_NAME\Zn
+"
+		if [ "$DEVICE_SELECTED" -eq 1 ]; then
+			TITLE="The selected device is"
+		else
+			TITLE="We detected this device as"
+		fi
+		DEVICE_CONFIRMATION="$TITLE:
+$DEVICE_LINE
+Flashing incorrect firmware will
+likely make your device unbootable.
+
+You can select your device from
+manually from the list.
+
+Proceed with the current device?"
+
+		dialog --yes-label 'Continue' --no-label 'Select' \
+		       --yesno "$DEVICE_CONFIRMATION" 0 0
+		if [ $? -eq 0 ] ; then
+			break
+		fi
+	else
+		DEVICE_WARNING="\Zb\Z1We could not detect your device\Zn
+
+Flashing incorrect firmware will
+likely make your device unbootable.
+
+Please CAREFULLY select your device
+manually from the list."
+
+		dialog --defaultno --yes-label 'Select' --no-label 'Exit' \
+		       --yesno "$DEVICE_WARNING" 0 0
+		if [ $? -ne 0 ] ; then
+			exit $?
+		fi
+	fi
+
+	exec 3>&1
+	NEW_DEVICE=$($THISDEVICE --devices $1 | while read i; do
+		echo $i | cut -d'-' -f1 | xargs
+		echo $i | cut -d'-' -f2- | xargs
+	done | tr \\n \\0 | xargs -0 \
+		dialog --menu "Select your device from the list" 0 0 3 \
+		2>&1 1>&3)
+	EXIT_STATUS=$?
+	exec 3>&-
+	if [ $EXIT_STATUS -ne 0 ] ; then
+		continue
+	fi
+
+	DEVICE=$NEW_DEVICE
+	DEVICE_SELECTED=1
+done
+
+# ----------
+# 2. Final confirmation
+# ----------
 DISCLAIMER="\Zb\Z3NOTICE\Zn
 
 While we carefully constructed this
@@ -16,30 +95,27 @@ you perform the update.
 
 Do you want to update now?"
 
-UP_TO_DATE=yes
-
-if [ -f "$DATE_FILE" ] ; then
-	DATE="`cat $DATE_FILE`"
-	export DIALOGOPTS="--colors --no-shadow --backtitle \"OpenDingux update $DATE\""
-fi
-
-echo "screen_color = (RED,RED,ON)" > /tmp/dialog_err.rc
-
 dialog --defaultno --yes-label 'Update' --no-label 'Cancel' --yesno "$DISCLAIMER" 0 0
 if [ $? -ne 0 ] ; then
 	exit $?
 fi
 
+# ----------
+# 3. Flashing
+# ----------
 clear
 echo 'Update in progress - please be patient.'
 echo
 
 if [ "$(whoami)" = "root" -a ! -x /usr/sbin/od-update ]; then
-	./od-update $1
+	PATH=$(pwd):$PATH FORCE_DEVICE="$DEVICE" ./od-update $1
 else
-	sudo od-update $1
+	sudo PATH=$(pwd):$PATH FORCE_DEVICE="$DEVICE" od-update $1
 fi
 
+# ----------
+# 4. Checking status
+# ----------
 ERR=$?
 if [ $ERR -ne 0 ] ; then
 	case $ERR in
@@ -92,6 +168,9 @@ if [ $ERR -ne 0 ] ; then
 	exit $ERR
 fi
 
+# ----------
+# 5. Reboot
+# ----------
 case $1 in
 	rs90)
 		LAST_KERNEL=START
